82bcd3b4e389ea9db13e1025d23fb04878ef1872
heylix <36211486+heylix@users.noreply.github.com>
2020-02-15 16:39:21 +0100

    git issue job auf RZL gitlab umgeschrieben
    nachdem das tuwat in unser eigenes gitlab importiert wurde, sollen natürlich die offenen Issues von dort abgeholt werden.
    
    relatiert: https://git.raumzeitlabor.org/rzl/tuwat/issues/212

--- jobs/github.rb
+++ jobs/github.rb
@@ -2,14 +2,14 @@ require 'rest-client'
 require 'json'
 require 'date'
 
-git_owner = "raumzeitlabor"
-git_project = "rzl-tuwat"
+git_owner = "rzl"
+git_project = "tuwat"
 
 ## Change this if you want to run more than one set of issue widgets
 event_name = "git_issues_labeled_defects"
 
 ## the endpoint we'll be hitting
-uri = "https://api.github.com/repos/#{git_owner}/#{git_project}"
+uri = "https://git.raumzeitlabor.org/api/v4/projects/#{git_owner}%2F#{git_project}
 
 SCHEDULER.every '2m', :first_in => 0 do |job|
     puts "Getting #{uri}"
